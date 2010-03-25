f922dee4bf52f86b976f5a98780886e309bf3624
mde <mde@localhost.(none)>
2010-03-25 06:19:44 +0000

    Added various routing examples as comments.

--- scripts/gen/router.js
+++ scripts/gen/router.js
@@ -2,6 +2,18 @@ var Router = require('geddy/lib/router').Router;
 
 router = new Router();
 router.match('/').to({controller: 'Main', action: 'index'});
-router.resource('users');
+
+// Basic routes
+// router.match('/moving/pictures/:id').to(
+//    {controller: 'Moving', action: 'pictures'});
+// router.match('/farewells/:farewelltype/kings/:kingid').to(
+//    {controller: 'Farewells', action: 'kings'});
+// Can also match specific HTTP methods only
+// router.match('/xandadu', 'get').to(
+//    {controller: 'Xandadu', action: 'specialHandler'});
+//
+// Resource-based routes
+// router.resource('hemispheres');
 
 exports.router = router;
+
