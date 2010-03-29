b13371772b6fc68365d6789b4f4a934d10310dc1
mde <mde@localhost.(none)>
2010-03-29 22:31:51 +0000

    Make app-startup fully async.

--- scripts/runserv.js
+++ scripts/runserv.js
@@ -4,13 +4,13 @@ var Config = require('geddy/lib/config').Config;
 var App = require('geddy/lib/app').App;
 var appDirname = process.argv[2];
 
-config = new Config(appDirname);
+var runServ = function (config) {
+  http.createServer(function (req, resp) {
+    new App(config).run(req, resp);
+  }).listen(8000);
 
-http.createServer(function (req, resp) {
-  new App(config).run(req, resp);
-}).listen(8000);
+  sys.puts('Server running at http://127.0.0.1:8000/');
+};
 
-
-sys.puts('Server running at http://127.0.0.1:8000/');
-
+new Config(appDirname, runServ);
 
