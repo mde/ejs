089f400a26f6be3fca8913a66d0905bea0cba1e3
mde <mde@localhost.(none)>
2010-04-03 07:12:49 +0000

    Added CouchDB storage for sessions.

--- scripts/runserv.js
+++ scripts/runserv.js
@@ -2,21 +2,23 @@ var appDirname = process.argv[2];
 
 var sys = require('sys');
 var http = require('http');
+
 var fleegix = require('geddy/lib/fleegix');
+
 var Config = require('geddy/lib/config').Config;
 var Init = require('geddy/lib/init').Init;
 var App = require('geddy/lib/app').App;
 
-config = new Config(appDirname);
-config = fleegix.mixin(config, require(config.dirname + '/config/config'));
-
-var runServ = function (initData) {
+var runServ = function () {
   http.createServer(function (req, resp) {
-    new App(initData).run(req, resp);
+    new App().run(req, resp);
   }).listen(config.port);
 
   sys.puts('Server running at http://127.0.0.1:' + config.port + '/');
 };
 
+var config = new Config(appDirname);
+// Initialize the app, passing in the config, and runServ at its callback
 new Init(config, runServ);
+
 
