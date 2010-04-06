373359e154611c08a7b0e06dc902e69ba727a4a8
mde <mde@localhost.(none)>
2010-04-06 22:45:51 +0000

    Default session store should be in-memory.

--- scripts/gen/config.js
+++ scripts/gen/config.js
@@ -2,9 +2,9 @@
 var config = new function () {
   //this.environment = 'development';
   //this.port = 8000;
-  this.plugins = [
-    'auth/auth'
-  ];
+  //this.plugins = [
+  //  'auth/auth'
+  //];
 }();
 
 for (var p in config) { this[p] = config[p]; }
