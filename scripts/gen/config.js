990a9a997d899cb9cc2f6e0456a5cac8e6e9cc9b
mde <mde@localhost.(none)>
2010-04-07 04:38:43 +0000

    Begin work on async, chainable auth methods.

--- scripts/gen/config.js
+++ scripts/gen/config.js
@@ -1,10 +1,20 @@
 
 var config = new function () {
-  //this.environment = 'development';
-  //this.port = 8000;
-  //this.plugins = [
-  //  'auth/auth'
-  //];
+  /*
+  this.environment = 'development';
+  this.port = 8000;
+  this.plugins = [
+    'auth/auth'
+  ];
+  this.sessions = {
+    store: 'couchdb',
+    key: 'sid',
+    expiry: 14 * 24 * 60 * 60,
+    dbHostname: 'localhost',
+    dbName: 'geddy_sessions',
+    dbPort: 5984
+  };
+  */
 }();
 
 for (var p in config) { this[p] = config[p]; }
