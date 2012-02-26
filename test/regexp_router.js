c9ef62901c1a8870e83b72e30db07536172dfc65
mde <mde@fleegix.org>
2012-02-26 12:25:11 -0800

    Fixed router test for resources, added HEAD requests to count.

--- test/regexp_router.js
+++ test/regexp_router.js
@@ -37,8 +37,8 @@ tests = {
   testCreateResource: function() {
     router = new Router();
     var routes = router.resource('SnowDogs');
-    assert.ok(routes.length === 7)
-    for ( var i in routes ) {
+    assert.ok(routes.length === 11)
+    for (var i in routes) {
       assert.ok(route)
     }
   },
