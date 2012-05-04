dff37b3c0570fb55d81b334b8d23fe8a947b390c
Meshach Jackson <meshachjackson@gmail.com>
2012-05-03 20:37:49 -0400

    repairing config settings for mongo example - removing adaptor deps

--- examples/todo_app_mongo/config/init.js
+++ examples/todo_app_mongo/config/init.js
@@ -1,8 +1,3 @@
-var mongo = require('mongodb-wrapper');
-
-geddy.db = mongo.db('localhost', 27017, 'todo');
-geddy.db.collection('todos');
-
 // Add uncaught-exception handler in prod-like environments
 if (geddy.config.environment != 'development') {
   process.addListener('uncaughtException', function (err) {
