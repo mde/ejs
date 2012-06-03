1557c4e73ebcfca2b2244de5b656e96fed65c5b9
Larz Conwell <larzconwell@gmail.com>
2012-06-03 01:57:58 -0400

    Geddy now loads other template engines(Not rendering yet), and included a Jade example

---
+++ examples/todo_app_jade/app/models/todo.js
@@ -0,0 +1,22 @@
+
+var Todo = function () {
+
+  this.defineProperties({
+    title: {type: 'string', required: true}
+  , id: {type: 'string', required: true}
+  , status: {type: 'string', required: true}
+  });
+
+  this.validatesPresent('title');
+  this.validatesLength('title', {min: 5});
+
+  this.validatesWithFunction('status', function (status) {
+    return status == 'open' || status == 'done';
+  });
+
+  this.adapter = "Mongo";
+
+};
+
+Todo = geddy.model.register('Todo', Todo);
+
---
+++ examples/todo_app_jade/config/init.js
@@ -0,0 +1,6 @@
+// Add uncaught-exception handler in prod-like environments
+if (geddy.config.environment != 'development') {
+  process.addListener('uncaughtException', function (err) {
+    geddy.log.error(JSON.stringify(err));
+  });
+}
