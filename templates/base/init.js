9086752b487dad2b2d45a5538923d8fe16cc4337
mde <mde@yammer-inc.com>
2012-08-22 21:43:09 -0700

    Much better default error-handling for uncaught exceptions

--- templates/base/init.js
+++ templates/base/init.js
@@ -2,7 +2,14 @@
 // Add uncaught-exception handler in prod-like environments
 if (geddy.config.environment != 'development') {
   process.addListener('uncaughtException', function (err) {
-    geddy.log.error(JSON.stringify(err));
+    var msg = err.message;
+    if (err.stack) {
+      msg += '\n' + err.stack;
+    }
+    if (!msg) {
+      msg = JSON.stringify(err);
+    }
+    geddy.log.error(msg);
   });
 }
 
