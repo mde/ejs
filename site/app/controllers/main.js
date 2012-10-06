60e83fef1ee16864e1da2380dd958300472cb90b
Techwraith <derickson@yammer-inc.com>
2012-10-06 15:56:56 -0700

    starting in on the github api crap for the site

--- site/app/controllers/main.js
+++ site/app/controllers/main.js
@@ -17,6 +17,14 @@
 */
 
 var Main = function () {
+
+  this.error = function (req, resp, params) {
+    self.respond(params, {
+      format: 'html'
+    , template: 'app/views/main/error'
+    });
+  };
+
   this.index = function (req, resp, params) {
     this.respond(params, {
       format: 'html'
@@ -25,10 +33,42 @@ var Main = function () {
   };
 
   this.documentation = function (req, resp, params) {
-    this.respond(params, {
-      format: 'html'
-    , template: 'app/views/main/documentation'
-    });
+    var self = this
+    , opts = {
+      url: 'https://api.github.com/repos/mde/geddy/commits'
+    , dataType: 'json'
+    }
+    , getTree = function (url, callback) {
+        var tree;
+        opts.url = url;
+        geddy.request(opts, function (err, trees) {
+          if (err || !trees) {
+            return this.error(req, resp, params);
+          }
+          for (var i in trees.tree) {
+            tree = trees.tree[i];
+            if (tree.path == 'docs') {
+              return getTree(tree.url, gotTree);
+            }
+          }
+          return callback(err, trees.tree);
+        });
+    }
+    , gotCommits = function (err, commits) {
+      var commit = commits[0] && commits[0].commit
+        , url = commit.tree && commit.tree.url;
+      return getTree(url, gotTree);
+    }
+    , gotTree = function (err, tree) {
+      for (var i in tree) {
+        console.log(tree[i].path, ':', tree[i].url);
+      }
+      self.respond(params, {
+        format: 'html'
+      , template: 'app/views/main/documentation'
+      });
+    }
+    geddy.request(opts, gotCommits);
   };
 
   this.tutorial = function (req, resp, params) {
