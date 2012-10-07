231a18d47607ec2149190d851f1df5a9fac488fb
Larz Conwell <larzconwell@gmail.com>
2012-10-07 09:17:26 -0400

    Closes #213, Templato no longer tries to use global template dependencies.

--- deps/templato/lib/engines/ejs.js
+++ deps/templato/lib/engines/ejs.js
@@ -1,8 +1,11 @@
-var TemplatoEJS = (function() {
+var path = require('path')
+  , TemplatoEJS;
+
+TemplatoEJS = (function() {
 
   function TemplatoEJS() {
     try {
-      this.engine = this.engine || require('ejs');
+      this.engine = this.engine || require(path.join(process.cwd(), 'node_modules', 'ejs'));
     } catch(err) {
       throw "To use EJS you will need to install it: [sudo] npm install [-g] ejs";
     }
