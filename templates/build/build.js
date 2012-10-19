0338f524189e16129ec30a57354b46d922201df2
Techwraith <derickson@yammer-inc.com>
2012-10-18 22:48:56 -0700

    started a buld process for front-end stuff

---
+++ templates/build/build.js
@@ -0,0 +1,12 @@
+window.geddy = {}
+
+// require model
+window.geddy.model = require('model');
+
+// mix utilities into geddy
+var utilities = require('utilities');
+utilities.mixin(geddy, utilities);
+
+// require socket.io-client
+window.geddy.io = require('socket.io-client');
+window.geddy.socket = window.geddy.io.connect('http://localhost');
