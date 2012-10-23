dbe729e360654af3c93c1a5fff3a7e745107330a
Techwraith <derickson@yammer-inc.com>
2012-10-22 17:53:00 -0700

    started work on the front end model feed routing

--- templates/build/build.js
+++ templates/build/build.js
@@ -10,3 +10,9 @@ utilities.mixin(geddy, utilities);
 // require socket.io-client
 window.geddy.io = require('socket.io-client');
 window.geddy.socket = window.geddy.io.connect('http://localhost');
+
+window.geddy.model.startListening = function (models) {
+  for (var i in models) {
+    // start here
+  }
+}
