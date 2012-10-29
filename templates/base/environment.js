35491b525859519666c8ecce9a2bf67eebe54495
mde <mde@yammer-inc.com>
2012-10-29 15:16:58 -0700

    Add stubs for Passport auth config

--- templates/base/environment.js
+++ templates/base/environment.js
@@ -4,6 +4,18 @@ var config = {
     port: 4001
   }
   */
+/* // For Passport auth via geddy-passport
+, passport: {
+    twitter: {
+      consumerKey: 'XXXXX'
+    , consumerSecret: 'XXXXX'
+    }
+  , facebook: {
+      clientID: 'XXXXX'
+    , clientSecret: 'XXXXX'
+    }
+  }
+*/
 };
 
 module.exports = config;
