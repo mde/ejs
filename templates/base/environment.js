7dbdced1d8ed6896709205a0aac9b7a02e14815d
mde <mde@yammer-inc.com>
2012-11-03 22:50:23 -0700

    Use secrets.json for secrets and keys, instead of environment.js

--- templates/base/environment.js
+++ templates/base/environment.js
@@ -4,20 +4,6 @@ var config = {
     port: 4001
   }
   */
-/* // For Passport auth via geddy-passport
-, passport: {
-    successRedirect: '/'
-  , failureRedirect: '/login'
-  , twitter: {
-      consumerKey: 'XXXXX'
-    , consumerSecret: 'XXXXX'
-    }
-  , facebook: {
-      clientID: 'XXXXX'
-    , clientSecret: 'XXXXX'
-    }
-  }
-*/
 };
 
 module.exports = config;
