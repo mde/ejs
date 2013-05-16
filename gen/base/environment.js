b66e44f0f5b5dc6e08a6ce51aaf3d7248ec1aee6
Ben Ng <omnomnomben@gmail.com>
2013-05-15 23:57:30 -0700

    Humanize flash messages (beyond assuming everything is a string)

--- gen/base/environment.js
+++ gen/base/environment.js
@@ -1,17 +1,5 @@
 var config = {
   /*
-  * Maps flash message keys to css classes
-  */
-  flashes: {
-    defaultClass: 'alert',
-    classes: {
-      success: 'alert alert-success',
-      alert: 'alert',
-      error: 'alert alert-error',
-      info: 'alert alert-info'
-    }
-  }
-  /*
   metrics: {
     port: 4001
   }
