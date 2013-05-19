7248dcf7d7a7fc98ff795d377720cd3717fee4f0
Ben Ng <omnomnomben@gmail.com>
2013-05-19 14:40:51 -0700

    Allow asynchronous init scripts

--- gen/base/init.js
+++ gen/base/init.js
@@ -1,15 +1,18 @@
+var init = function(cb) {
+  // Add uncaught-exception handler in prod-like environments
+  if (geddy.config.environment != 'development') {
+    process.addListener('uncaughtException', function (err) {
+      var msg = err.message;
+      if (err.stack) {
+        msg += '\n' + err.stack;
+      }
+      if (!msg) {
+        msg = JSON.stringify(err);
+      }
+      geddy.log.error(msg);
+    });
+  }
+  cb();
+};
 
-// Add uncaught-exception handler in prod-like environments
-if (geddy.config.environment != 'development') {
-  process.addListener('uncaughtException', function (err) {
-    var msg = err.message;
-    if (err.stack) {
-      msg += '\n' + err.stack;
-    }
-    if (!msg) {
-      msg = JSON.stringify(err);
-    }
-    geddy.log.error(msg);
-  });
-}
-
+exports.init = init;
