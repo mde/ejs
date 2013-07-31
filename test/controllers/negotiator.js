20e0194f3ec4645da19805847c1c96fbf575656f
mde <mde@fleegix.org>
2013-07-30 23:48:33 -0700

    Added test for content-negotiation

---
+++ test/controllers/negotiator.js
@@ -0,0 +1,66 @@
+var assert = require('assert')
+  , util = require('utilities')
+  , Negotiator = require('../../lib/controller/responder/negotiator').Negotiator
+  // For unit testing purposes we'll inject this shim in place of a real controller
+  , ControllerShim = require('./shim')
+  , tests;
+
+tests = {
+  'negotiate defaults to the first format if there is no requested format': function () {
+    var n = new Negotiator('*/*', ['html', 'text'], null, {});
+    assert.equal('html', n.negotiate().format);
+  }
+
+, 'negotiate will use the requested format set on negotiator': function () {
+    var n = new Negotiator('*/*', ['html', 'txt'], 'txt', {});
+    assert.equal('txt', n.negotiate().format);
+  }
+
+, 'negotiate will throw when controller does not support default format': function () {
+    var n = new Negotiator('*/*', ['html', 'json'], 'txt', {});
+    assert.throws(function () {
+      n.negotiate();
+    });
+  }
+
+, 'passed-in format overrides controller\'s acceppted formats': function () {
+    var n = new Negotiator('*/*', ['html', 'json'], 'txt', {});
+    assert.equal('xml', n.negotiate('xml').format);
+  }
+
+, 'negotiator will pick first client accept format that controller supports': function () {
+    var n = new Negotiator('application/xml,application/xhtml+xml,' +
+          'text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
+          ['xml', 'html', 'json'], null, {});
+    assert.equal('xml', n.negotiate().format);
+  }
+
+, 'negotiator will pick first client accept format that matches requested param': function () {
+    var n = new Negotiator('application/xml,application/xhtml+xml,' +
+          'text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
+          ['xml', 'html', 'json'], 'html', {});
+    assert.equal('html', n.negotiate().format);
+  }
+
+, 'negotiator will throw if no client accept format matches requested param': function () {
+    var n = new Negotiator('application/xml,application/xhtml+xml,' +
+          'text/plain;q=0.8,image/png',
+          ['xml', 'html', 'json'], 'html', {});
+    assert.throws(function () {
+      n.negotiate();
+    });
+  }
+
+, 'passed-in format overrides controller\'s acceppted formats, ' +
+      'but has to match client accepts header': function () {
+    var n = new Negotiator('application/xml,application/xhtml+xml,' +
+          'text/plain;q=0.8,image/png',
+          ['xml', 'html', 'json'], 'html', {});
+    assert.throws(function () {
+      n.negotiate('html');
+    });
+  }
+
+}
+
+module.exports = tests;
