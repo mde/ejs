c75f25a0a465012f9820f0fb59dc837131f61c21
mde <mde@fleegix.org>
2013-07-31 15:40:58 -0700

    Refactor negotiator

--- test/controllers/negotiator.js
+++ test/controllers/negotiator.js
@@ -7,45 +7,45 @@ var assert = require('assert')
 
 tests = {
   'negotiate defaults to the first format if there is no requested format': function () {
-    var n = new Negotiator('*/*', ['html', 'text'], null, {});
+    var n = new Negotiator('*/*', ['html', 'text'], null);
     assert.equal('html', n.negotiate().format);
   }
 
 , 'negotiate will use the requested format set on negotiator': function () {
-    var n = new Negotiator('*/*', ['html', 'txt'], 'txt', {});
+    var n = new Negotiator('*/*', ['html', 'txt'], 'txt');
     assert.equal('txt', n.negotiate().format);
   }
 
 , 'negotiate will throw when controller does not support default format': function () {
-    var n = new Negotiator('*/*', ['html', 'json'], 'txt', {});
+    var n = new Negotiator('*/*', ['html', 'json'], 'txt');
     assert.throws(function () {
       n.negotiate();
     });
   }
 
 , 'passed-in format overrides controller\'s acceppted formats': function () {
-    var n = new Negotiator('*/*', ['html', 'json'], 'txt', {});
+    var n = new Negotiator('*/*', ['html', 'json'], 'txt');
     assert.equal('xml', n.negotiate('xml').format);
   }
 
-, 'negotiator will pick first client accept format that controller supports': function () {
+, 'if no requested param, negotiator will pick first client accept format that controller supports': function () {
     var n = new Negotiator('application/xml,application/xhtml+xml,' +
           'text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
-          ['xml', 'html', 'json'], null, {});
+          ['xml', 'html', 'json'], null);
     assert.equal('xml', n.negotiate().format);
   }
 
 , 'negotiator will pick first client accept format that matches requested param': function () {
     var n = new Negotiator('application/xml,application/xhtml+xml,' +
           'text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
-          ['xml', 'html', 'json'], 'html', {});
+          ['xml', 'html', 'json'], 'html');
     assert.equal('html', n.negotiate().format);
   }
 
 , 'negotiator will throw if no client accept format matches requested param': function () {
     var n = new Negotiator('application/xml,application/xhtml+xml,' +
           'text/plain;q=0.8,image/png',
-          ['xml', 'html', 'json'], 'html', {});
+          ['xml', 'html', 'json'], 'html');
     assert.throws(function () {
       n.negotiate();
     });
