2376c9b87e237168364c99e45a547674240c7415
mde <mde@fleegix.org>
2015-01-03 22:48:13 -0800

    Get all v1 tests passing

--- test/ejs.js
+++ test/ejs.js
@@ -1,348 +1,300 @@
-/*
- * Geddy JavaScript Web development framework
- * Copyright 2112 Matthew Eernisse (mde@fleegix.org)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
-*/
+/**
+ * Module dependencies.
+ */
 
-var ejs = require('../lib/ejs')
-  , assert = require('assert')
-  , tests
-  , render = ejs.render;
+var ejs = require('..')
+  , fs = require('fs')
+  , read = fs.readFileSync
+  , assert = require('assert');
 
-tests = {
+/**
+ * Load fixture `name`.
+ */
 
-  'compiles a function': function () {
-    var str = '<p>howdy</p>'
-      , fn = ejs.compile(str);
-    assert.equal(str, fn());
-  }
+function fixture(name) {
+  return read('test/fixtures/' + name, 'utf8').replace(/\r/g, '').trim();
+}
 
-, 'custom delimiter passed to render': function () {
-    var str = "<&= foo; &>"
-      , actual = render(str, {foo: 'FOO'}, {delimiter: '&'});
-    assert.equal('FOO', actual);
-  }
+/**
+ * User fixtures.
+ */
 
-, 'custom delimiter globally': function () {
-    ejs.delimiter = '$';
-    var str = "<$= foo; $>"
-      , actual = render(str, {foo: 'FOO'});
-    assert.equal('FOO', actual);
+var users = [];
+users.push({name: 'geddy'});
+users.push({name: 'neil'});
+users.push({name: 'alex'});
+
+suite('ejs.compile(str, options)', function () {
+  test('compile to a function', function () {
+    var fn = ejs.compile('<p>yay</p>');
+    assert.equal(fn(), '<p>yay</p>');
+  })
+
+  test('throw if there are syntax errors', function () {
+    try {
+      ejs.compile(fixture('fail.ejs'));
+    }
+    catch (err) {
+      assert.ok(err.message.indexOf('compiling ejs') > -1);
+
+      try {
+        ejs.compile(fixture('fail.ejs'), {filename: 'fail.ejs'});
+      }
+      catch (err) {
+        console.log(err);
+        assert.ok(err.message.indexOf('fail.ejs') > -1);
+        return;
+      }
+    }
+  })
+
+  test('allow customizing delimiter local var', function () {
+    var fn = ejs.compile('<p><?= name ?></p>', {delimiter: '?'});
+    assert.equal(fn({name: 'geddy'}), '<p>geddy</p>');
+
+    var fn = ejs.compile('<p><:= name :></p>', {delimiter: ':'});
+    assert.equal(fn({name: 'geddy'}), '<p>geddy</p>');
+
+    var fn = ejs.compile('<p><$= name $></p>', {delimiter: '$'});
+    assert.equal(fn({name: 'geddy'}), '<p>geddy</p>');
+  })
+
+  test('default to using ejs.delimiter', function () {
+    ejs.delimiter = '&';
+    var fn = ejs.compile('<p><&= name &></p>');
+    assert.equal(fn({name: 'geddy'}), '<p>geddy</p>');
+
+    var fn = ejs.compile('<p><|= name |></p>', {delimiter: '|'});
+    assert.equal(fn({name: 'geddy'}), '<p>geddy</p>');
     delete ejs.delimiter;
-  }
+  })
 
-, 'client produces a standalone function': function () {
+  test('have a working client option', function () {
     var fn = ejs.compile('<p><%= foo %></p>', {client: true});
     var str = fn.toString();
     eval('var preFn = ' + str);
-    assert.equal('<p>bar</p>', preFn({foo: 'bar'}));
-  }
+    assert.equal(preFn({foo: 'bar'}), '<p>bar</p>');
+  })
+})
 
-, 'empty template render': function () {
-    var actual = render('');
-    assert.equal('', actual);
-  }
+suite('ejs.render(str, data)', function () {
+  test('render the template', function () {
+    assert.equal(ejs.render('<p>yay</p>'), '<p>yay</p>');
+  })
 
-, 'rendering a single variable': function () {
-    var str = "<% var foo = 'FOO'; %><%= foo; %>"
-      , actual = render(str);
-    assert.equal('FOO', actual);
-  }
+  test('accept locals', function () {
+    assert.equal(ejs.render('<p><%= name %></p>', {name: 'geddy'}),
+        '<p>geddy</p>');
+  })
+})
 
-, 'rendering passed-in data': function () {
-    var str = "<%= foo; %>"
-      , actual = render(str, {foo: 'FOO'});
-    assert.equal('FOO', actual);
-  }
-
-, 'renderFile renders a template file': function (next) {
-    ejs.renderFile('test/fixtures/file.ejs', function (err, html) {
-      if (err) {
-        throw err;
-      }
-      assert.equal('<p>w00t</p>', html);
-      next();
+suite('ejs.renderFile(path, options, fn)', function () {
+  test('render a file', function(done) {
+    ejs.renderFile('test/fixtures/para.ejs', function(err, html) {
+      if (err) return done(err);
+      assert.equal(html, '<p>hey</p>');
+      done();
     });
-  }
+  })
 
-, 'literal EJS': function () {
-    var str = "<%% var foo = 'FOO'; %>"
-      , actual = render(str);
-    assert.equal("<% var foo = 'FOO'; %>", actual);
-  }
+  test('accept locals', function(done) {
+    var data =  {name: 'fonebone'}
+      , options = {delimiter: '$'};
+    ejs.renderFile('test/fixtures/user.ejs', data, options, function(err, html) {
+      if (err) return done(err);
+      assert.equal(html, '<h1>fonebone</h1>');
+      done();
+    });
+  })
 
-, 'comments': function () {
-    var str = "<%# Blah blah blah %><% var foo = 'FOO'; %><%= foo; %>"
-      , actual = render(str);
-    assert.equal('FOO', actual);
-  }
+  test('not catch err thrown by callback', function(done) {
+    var data =  {name: 'fonebone'}
+      , options = {delimiter: '$'}
+      , counter = 0;
 
-, 'escaping': function () {
-    var html;
-    html = ejs.render('<%= name %>', {name: '&nbsp;<script>'});
-    assert.equal('&amp;nbsp;&lt;script&gt;', html);
-    html = ejs.render('<%= name %>', {name: "The Jones's"});
-    assert.equal('The Jones&#39;s', html);
-    html = ejs.render('<%= name %>', {name: "&foo_bar;"});
-    assert.equal('&amp;foo_bar;', html);
-  }
+    var d = require('domain').create();
+    d.on('error', function (err) {
+      assert.equal(counter, 1);
+      assert.equal(err.message, 'Exception in callback');
+      done();
+    });
+    d.run(function () {
+      ejs.renderFile('test/fixtures/user.ejs', data, options, function(err, html) {
+        counter++;
+        if (err) {
+          assert.notEqual(err.message, 'Exception in callback');
+          return done(err);
+        }
+        throw new Error('Exception in callback');
+      });
+    });
+  })
+})
 
-, 'no escaping': function () {
-    var val = '&nbsp;<script>'
-      , html = ejs.render('<%- name %>', {name: val});
-    assert.equal(val, html);
-  }
+suite('<%=', function () {
 
-, 'handle no closing tag': function () {
+  test('escape &amp;<script>', function () {
+    assert.equal(ejs.render('<%= name %>', {name: '&nbsp;<script>'}),
+        '&amp;nbsp;&lt;script&gt;');
+  })
+
+  test("should escape '", function () {
+    assert.equal(ejs.render('<%= name %>', {name: "The Jones's"}),
+      'The Jones&#39;s');
+  })
+  
+  test("should escape &foo_bar;", function () {
+    assert.equal(ejs.render('<%= name %>', {name: "&foo_bar;"}),
+      '&amp;foo_bar;');
+  })
+})
+
+suite('<%-', function () {
+  test('not escape', function () {
+    assert.equal(ejs.render('<%- name %>', {name: '<script>'}),
+        '<script>');
+  })
+
+  test('terminate gracefully if no close tag is found', function () {
     try {
-      ejs.render('foo <%= name bar', {name: 'Bytor'});
-    }
-    catch(e) {
-      assert.ok(e.message.indexOf('Could not find matching close tag') > -1);
-    }
-  }
-
-, 'HTML equality': function () {
-    assert.equal('<p>yay</p>', render('<p>yay</p>'));
-  }
-
-, 'basic conditional': function () {
-    var str = '<% if (name) { %><p><%= name %></p><% } %>'
-      , actual = render(str, {name: 'mde'});
-    assert.equal('<p>mde</p>', actual);
-  }
-
-, 'newlines': function () {
-    var html = '\n<p>mde</p>\n<p>mde@fleegix.org</p>\n'
-      , str = '<% if (name) { %>\n<p><%= name %></p>\n<p><%= email %></p>\n<% } %>'
-      , data = { name: 'mde', email: 'mde@fleegix.org' };
-    assert.equal(html, render(str, data));
-  }
-
-, 'single quotes': function () {
-    var html = '<p>WAHOO</p>'
-      , str = "<p><%= up('wahoo') %></p>"
-      , data = {up: function (str) { return str.toUpperCase(); }};
-    assert.equal(html, render(str, data));
-  }
-
-, 'single quotes in HTML': function () {
-    var html = '<p>WAHOO that\'s cool</p>'
-      , str = '<p><%= up(\'wahoo\') %> that\'s cool</p>'
-      , data = {up: function (str) { return str.toUpperCase(); }};
-    assert.equal(html, render(str, data));
-  }
-
-, 'multiple single quotes': function () {
-    var html = "<p>couldn't shouldn't can't</p>"
-      , str = "<p>couldn't shouldn't can't</p>";
-    assert.equal(html, render(str));
-  }
-
-, 'single quotes inside tags': function () {
-    var html = '<p>string</p>'
-      , str = "<p><%= 'string' %></p>";
-    assert.equal(html, render(str));
-  }
-
-, 'back-slashes in the document': function () {
-    var html = "<p>backslash: '\\'</p>"
-      , str = "<p>backslash: '\\'</p>";
-    assert.equal(html, render(str));
-  }
-
-, 'double quotes': function () {
-    var html = '<p>WAHOO</p>'
-      , str = '<p><%= up("wahoo") %></p>'
-      , data = {up: function (str) { return str.toUpperCase(); }};
-    assert.equal(html, render(str, data));
-  }
-
-, 'multiple double quotes': function () {
-    var html = '<p>just a "test" wahoo</p>'
-      , str = '<p>just a "test" wahoo</p>';
-    assert.equal(html, render(str));
-  }
-
-, 'whitespace': function () {
-    var users, html, str;
-
-    html = '<p>foo</p>'
-    str = '<p><%="foo"%></p>';
-    assert.equal(html, render(str));
-
-    html = '<p>foo</p>';
-    str = '<p><%=bar%></p>';
-    assert.equal(html, render(str, {bar: 'foo'}));
-
-    users = ['geddy', 'neil', 'alex'];
-    html = '<ul><li>geddy</li><li>neil</li><li>alex</li></ul>';
-    str = '<ul><%users.forEach(function(user){%><li><%=user%></li><%})%></ul>';
-    assert.equal(html, render(str, {users: users}));
-  }
-
-, 'iteration': function () {
-    var html = '<p>foo</p>',
-      str = '<% for (var key in items) { %>'
-        + '<p><%= items[key] %></p>'
-        + '<% } %>';
-    assert.equal(html, render(str, {items: ['foo']}));
-
-    var html = '<p>foo</p>',
-      str = '<% items.forEach(function (item) { %>'
-        + '<p><%= item %></p>'
-        + '<% }) %>';
-    assert.equal(html, render(str, {items: ['foo']}));
-  }
-
-, 'syntax error': function () {
-    var str = '<% new Date(1 2); %>';
-    try {
-      render(str, {});
-    }
-    catch(e) {
-      assert.ok(e.message.indexOf('while compiling EJS') > -1);
-    }
-  }
-
-, 'useful stack traces': function () {
-    var str = [
-      "A little somethin'",
-      "somethin'",
-      "<% if (name) { %>", // Failing line
-      "  <p><%= name %></p>",
-      "  <p><%= email %></p>",
-      "<% } %>"
-    ].join("\n");
-
-    try {
-      render(str, {}, {filename: 'asdf.js'})
+      ejs.compile('<h1>oops</h1><%- name ->')
+      throw new Error('Expected parse failure');
     }
     catch (err) {
-      assert.ok(err.message.indexOf("name is not defined") > -1);
-      assert.deepEqual(err.name, "ReferenceError");
-      var lineno = parseInt(err.toString().match(/asdf.js:(\d+)\n/)[1]);
-      assert.deepEqual(lineno, 3,
-          "Error should been thrown on line 3, was thrown on line "+ lineno);
+      assert.ok(err.message.indexOf('Could not find matching close tag for') > -1);
     }
-  }
+  })
+})
 
-, 'useful stack traces multiline': function () {
-    var str = [
-      "A little somethin'",
-      "somethin'",
-      "<% var some = 'pretty';",
-      "   var multiline = 'javascript';",
-      "%>",
-      "<% if (name) { %>", // Failing line
-      "  <p><%= name %></p>",
-      "  <p><%= email %></p>",
-      "<% } %>"
-    ].join("\n");
+suite('%>', function () {
+  test('produce newlines', function () {
+    assert.equal(ejs.render(fixture('newlines.ejs'), {users: users}),
+      fixture('newlines.html'));
+  })
+})
 
+suite('-%>', function () {
+  test('not produce newlines', function () {
+    assert.equal(ejs.render(fixture('no.newlines.ejs'), {users: users}),
+      fixture('no.newlines.html'));
+  })
+})
+
+suite('<%%', function () {
+  test('produce literals', function () {
+    assert.equal(ejs.render('<%%- "foo" %>'),
+      '<%- "foo" %>');
+  })
+})
+
+suite('single quotes', function () {
+  test('not mess up the constructed function', function () {
+    assert.equal(ejs.render(fixture('single-quote.ejs')),
+      fixture('single-quote.html'));
+  })
+})
+
+suite('double quotes', function () {
+  test('not mess up the constructed function', function () {
+    assert.equal(ejs.render(fixture('double-quote.ejs')),
+      fixture('double-quote.html'));
+  })
+})
+
+suite('backslashes', function () {
+  test('escape', function () {
+    assert.equal(ejs.render(fixture('backslash.ejs')),
+      fixture('backslash.html'));
+  })
+})
+
+suite('messed up whitespace', function () {
+  test('work', function () {
+    assert.equal(ejs.render(fixture('messed.ejs'), {users: users}),
+      fixture('messed.html'));
+  })
+})
+
+suite('exceptions', function () {
+  test('produce useful stack traces', function () {
     try {
-      render(str)
+      ejs.render(fixture('error.ejs'), {filename: 'error.ejs'});
     }
     catch (err) {
-      assert.ok(err.message.indexOf("name is not defined") > -1);
-      assert.deepEqual(err.name, "ReferenceError");
-      var lineno = parseInt(err.toString().match(/ejs:(\d+)\n/)[1]);
-      assert.deepEqual(lineno, 6,
-          "Error should been thrown on line 6, was thrown on line "+ lineno);
+      assert.equal(err.path, 'error.ejs');
+      assert.equal(err.stack.split('\n').slice(0, 8).join('\n'), fixture('error.out'));
     }
-  }
+  })
 
-, 'no compileDebug': function () {
-    var str = '<% if (foo) {} %>';
+  test('not include fancy stack info if compileDebug is false', function () {
     try {
-      render(str, {}, {compileDebug: false, filename: 'asdf.js'});
+      ejs.render(fixture('error.ejs'), {}, {
+        filename: 'error.ejs',
+        compileDebug: false
+      });
     }
-    catch(e) {
-      assert.ok(!e.path);
+    catch (err) {
+      assert.ok(!err.path);
+      assert.notEqual(err.stack.split('\n').slice(0, 8).join('\n'), fixture('error.out'));
     }
-  }
+  });
+})
 
-, 'slurp' : function () {
-    var expected = 'me\nhere'
-      , str = 'me<% %>\nhere';
-    assert.equal(expected, render(str));
+suite('includes', function () {
+  test('include ejs', function () {
+    var file = 'test/fixtures/include.ejs';
+    assert.equal(ejs.render(fixture('include.ejs'), {pets: users}, {filename: file, delimiter: '@'}),
+        fixture('include.html'));
+  })
 
-    var expected = 'mehere'
-      , str = 'me<% -%>\nhere';
-    assert.equal(expected, render(str));
+  test('work when nested', function () {
+    var file = 'test/fixtures/menu.ejs';
+    assert.equal(ejs.render(fixture('menu.ejs'), {pets: users}, {filename: file}),
+        fixture('menu.html'));
+  })
 
-    var expected = 'me\nhere'
-      , str = 'me<% -%>\n\nhere';
-    assert.equal(expected, render(str));
+  test('work with a variable path', function () {
+    var file = 'test/fixtures/menu_var.ejs',
+        includePath = 'includes/menu-item';
+    assert.equal(ejs.render(fixture('menu.ejs'), {pets: users, varPath:  includePath}, {filename: file}),
+      fixture('menu.html'));
+  })
 
-    var expected = 'me inbetween \nhere'
-      , str = 'me <%= x %> \nhere';
-    assert.equal(expected, render(str,{x:'inbetween'}));
+  test('include arbitrary files as-is', function () {
+    var file = 'test/fixtures/include.css.ejs';
+    assert.equal(ejs.render(fixture('include.css.ejs'), {pets: users}, {filename: file}),
+        fixture('include.css.html'));
+  })
 
-    var expected = 'me inbetween here'
-      , str = 'me <%= x -%> \nhere';
-    assert.equal(expected, render(str, {x:'inbetween'}));
+  test('pass compileDebug to include', function () {
+    var file = 'test/fixtures/include.ejs';
+    var fn = ejs.compile(fixture('include.ejs'), {
+      filename: file
+    , delimiter: '@'
+    , compileDebug: false
+    , client: true
+    });
+    var str = fn.toString();
+    eval('var preFn = ' + str);
+    assert.ok(!str.match(/__filename/));
+    assert.doesNotThrow(function () {
+      preFn({pets: users});
+    });
+  })
+})
 
-    var expected = 'me <p>inbetween</p> here'
-      , str = 'me <%- x -%> \nhere';
-    assert.equal(expected, render(str,{x:'<p>inbetween</p>'}));
+suite('comments', function () {
+  test('fully render with comments removed', function () {
+    assert.equal(ejs.render(fixture('comments.ejs')),
+        fixture('comments.html'));
+  })
+})
 
-    var expected = '\n  Hallo 0\n\n  Hallo 1\n\n'
-      , str = '<% for (var i in [1,2]) { %>\n' +
-            '  Hallo <%= i %>\n' +
-            '<% } %>\n';
-    assert.equal(expected, render(str));
-
-    var expected = '  Hallo 0\n  Hallo 1\n'
-      , str = '<% for (var i in [1,2]) { -%>\n' +
-            '  Hallo <%= i %>\n' +
-            '<% } -%>\n';
-    assert.equal(expected, render(str));
-  }
-
-, 'test include directive': function () {
-    var tmpl = '<% include fixtures/include %>'
-      , html = render(tmpl, {foo: true}, {filename: './test/ejs.js'});
-    assert.equal('foo: true', html);
-  }
-
-, 'test nested include directive': function () {
-    var tmpl = '<% include fixtures/include_a %>'
-      , html = render(tmpl, {bar: true}, {filename: './test/ejs.js'});
-    assert.equal('Howdy bar: true', html);
-  }
-
-, 'test include function': function () {
-    var tmpl = '<%= include("fixtures/include"); %>'
-      , html = render(tmpl, {foo: true}, {filename: './test/ejs.js'});
-    assert.equal('foo: true', html);
-  }
-
-, 'test include function variable path': function () {
-    var tmpl = '<%= include(includePath); %>'
-      , html = render(tmpl, {foo: true, includePath: "fixtures/include"},
-            {filename: './test/ejs.js'});
-    assert.equal('foo: true', html);
-  }
-
-, 'require support': function () {
+suite('require', function () {
+  test('allow ejs templates to be required as node modules', function () {
       var file = 'test/fixtures/include.ejs'
-        , template = require('./fixtures/include.ejs');
-      assert.equal('foo: true', template({foo: true}));
-  }
-
-};
-
-module.exports = tests;
+        , template = require(__dirname + '/fixtures/menu.ejs');
+      assert.equal(template({filename: file, pets: users}),
+        fixture('menu.html'));
+  })
+})
