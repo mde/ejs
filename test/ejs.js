e4c2c573cc39d253e3ec5ae1b9c5d341f21c6778
mde <mde@fleegix.org>
2015-01-04 19:33:50 -0800

    Linting

--- test/ejs.js
+++ test/ejs.js
@@ -28,7 +28,7 @@ suite('ejs.compile(str, options)', function () {
   test('compile to a function', function () {
     var fn = ejs.compile('<p>yay</p>');
     assert.equal(fn(), '<p>yay</p>');
-  })
+  });
 
   test('throw if there are syntax errors', function () {
     try {
@@ -46,66 +46,75 @@ suite('ejs.compile(str, options)', function () {
         return;
       }
     }
-  })
+  });
 
   test('allow customizing delimiter local var', function () {
-    var fn = ejs.compile('<p><?= name ?></p>', {delimiter: '?'});
+    var fn;
+    fn = ejs.compile('<p><?= name ?></p>', {delimiter: '?'});
     assert.equal(fn({name: 'geddy'}), '<p>geddy</p>');
 
-    var fn = ejs.compile('<p><:= name :></p>', {delimiter: ':'});
+    fn = ejs.compile('<p><:= name :></p>', {delimiter: ':'});
     assert.equal(fn({name: 'geddy'}), '<p>geddy</p>');
 
-    var fn = ejs.compile('<p><$= name $></p>', {delimiter: '$'});
+    fn = ejs.compile('<p><$= name $></p>', {delimiter: '$'});
     assert.equal(fn({name: 'geddy'}), '<p>geddy</p>');
-  })
+  });
 
   test('default to using ejs.delimiter', function () {
+    var fn;
     ejs.delimiter = '&';
-    var fn = ejs.compile('<p><&= name &></p>');
+    fn = ejs.compile('<p><&= name &></p>');
     assert.equal(fn({name: 'geddy'}), '<p>geddy</p>');
 
-    var fn = ejs.compile('<p><|= name |></p>', {delimiter: '|'});
+    fn = ejs.compile('<p><|= name |></p>', {delimiter: '|'});
     assert.equal(fn({name: 'geddy'}), '<p>geddy</p>');
     delete ejs.delimiter;
-  })
+  });
 
   test('have a working client option', function () {
-    var fn = ejs.compile('<p><%= foo %></p>', {client: true});
-    var str = fn.toString();
+    var fn
+      , str
+      , preFn;
+    fn = ejs.compile('<p><%= foo %></p>', {client: true});
+    str = fn.toString();
     eval('var preFn = ' + str);
     assert.equal(preFn({foo: 'bar'}), '<p>bar</p>');
-  })
-})
+  });
+});
 
 suite('ejs.render(str, data)', function () {
   test('render the template', function () {
     assert.equal(ejs.render('<p>yay</p>'), '<p>yay</p>');
-  })
+  });
 
   test('accept locals', function () {
     assert.equal(ejs.render('<p><%= name %></p>', {name: 'geddy'}),
         '<p>geddy</p>');
-  })
-})
+  });
+});
 
 suite('ejs.renderFile(path, options, fn)', function () {
   test('render a file', function(done) {
     ejs.renderFile('test/fixtures/para.ejs', function(err, html) {
-      if (err) return done(err);
+      if (err) {
+        return done(err);
+      }
       assert.equal(html, '<p>hey</p>');
       done();
     });
-  })
+  });
 
   test('accept locals', function(done) {
     var data =  {name: 'fonebone'}
       , options = {delimiter: '$'};
     ejs.renderFile('test/fixtures/user.ejs', data, options, function(err, html) {
-      if (err) return done(err);
+      if (err) {
+        return done(err);
+      }
       assert.equal(html, '<h1>fonebone</h1>');
       done();
     });
-  })
+  });
 
   test('not catch err thrown by callback', function(done) {
     var data =  {name: 'fonebone'}
@@ -128,92 +137,92 @@ suite('ejs.renderFile(path, options, fn)', function () {
         throw new Error('Exception in callback');
       });
     });
-  })
-})
+  });
+});
 
 suite('<%=', function () {
 
   test('escape &amp;<script>', function () {
     assert.equal(ejs.render('<%= name %>', {name: '&nbsp;<script>'}),
         '&amp;nbsp;&lt;script&gt;');
-  })
+  });
 
   test("should escape '", function () {
     assert.equal(ejs.render('<%= name %>', {name: "The Jones's"}),
       'The Jones&#39;s');
-  })
+  });
   
   test("should escape &foo_bar;", function () {
     assert.equal(ejs.render('<%= name %>', {name: "&foo_bar;"}),
       '&amp;foo_bar;');
-  })
-})
+  });
+});
 
 suite('<%-', function () {
   test('not escape', function () {
     assert.equal(ejs.render('<%- name %>', {name: '<script>'}),
         '<script>');
-  })
+  });
 
   test('terminate gracefully if no close tag is found', function () {
     try {
-      ejs.compile('<h1>oops</h1><%- name ->')
+      ejs.compile('<h1>oops</h1><%- name ->');
       throw new Error('Expected parse failure');
     }
     catch (err) {
       assert.ok(err.message.indexOf('Could not find matching close tag for') > -1);
     }
-  })
-})
+  });
+});
 
 suite('%>', function () {
   test('produce newlines', function () {
     assert.equal(ejs.render(fixture('newlines.ejs'), {users: users}),
       fixture('newlines.html'));
-  })
-})
+  });
+});
 
 suite('-%>', function () {
   test('not produce newlines', function () {
     assert.equal(ejs.render(fixture('no.newlines.ejs'), {users: users}),
       fixture('no.newlines.html'));
-  })
-})
+  });
+});
 
 suite('<%%', function () {
   test('produce literals', function () {
     assert.equal(ejs.render('<%%- "foo" %>'),
       '<%- "foo" %>');
-  })
-})
+  });
+});
 
 suite('single quotes', function () {
   test('not mess up the constructed function', function () {
     assert.equal(ejs.render(fixture('single-quote.ejs')),
       fixture('single-quote.html'));
-  })
-})
+  });
+});
 
 suite('double quotes', function () {
   test('not mess up the constructed function', function () {
     assert.equal(ejs.render(fixture('double-quote.ejs')),
       fixture('double-quote.html'));
-  })
-})
+  });
+});
 
 suite('backslashes', function () {
   test('escape', function () {
     assert.equal(ejs.render(fixture('backslash.ejs')),
       fixture('backslash.html'));
-  })
-})
+  });
+});
 
 suite('messed up whitespace', function () {
   test('work', function () {
     assert.equal(ejs.render(fixture('messed.ejs'), {users: users}),
       fixture('messed.html'));
-  })
-})
+  });
+});
 
 suite('exceptions', function () {
   test('produce useful stack traces', function () {
@@ -224,7 +233,7 @@ suite('exceptions', function () {
       assert.equal(err.path, 'error.ejs');
       assert.equal(err.stack.split('\n').slice(0, 8).join('\n'), fixture('error.out'));
     }
-  })
+  });
 
   test('not include fancy stack info if compileDebug is false', function () {
     try {
@@ -238,57 +247,60 @@ suite('exceptions', function () {
       assert.notEqual(err.stack.split('\n').slice(0, 8).join('\n'), fixture('error.out'));
     }
   });
-})
+});
 
 suite('includes', function () {
   test('include ejs', function () {
     var file = 'test/fixtures/include.ejs';
     assert.equal(ejs.render(fixture('include.ejs'), {pets: users}, {filename: file, delimiter: '@'}),
         fixture('include.html'));
-  })
+  });
 
   test('work when nested', function () {
     var file = 'test/fixtures/menu.ejs';
     assert.equal(ejs.render(fixture('menu.ejs'), {pets: users}, {filename: file}),
         fixture('menu.html'));
-  })
+  });
 
   test('work with a variable path', function () {
     var file = 'test/fixtures/menu_var.ejs',
         includePath = 'includes/menu-item';
     assert.equal(ejs.render(fixture('menu.ejs'), {pets: users, varPath:  includePath}, {filename: file}),
       fixture('menu.html'));
-  })
+  });
 
   test('include arbitrary files as-is', function () {
     var file = 'test/fixtures/include.css.ejs';
     assert.equal(ejs.render(fixture('include.css.ejs'), {pets: users}, {filename: file}),
         fixture('include.css.html'));
-  })
+  });
 
   test('pass compileDebug to include', function () {
-    var file = 'test/fixtures/include.ejs';
-    var fn = ejs.compile(fixture('include.ejs'), {
+    var file = 'test/fixtures/include.ejs'
+      , fn
+      , str
+      , preFn;
+    fn = ejs.compile(fixture('include.ejs'), {
       filename: file
     , delimiter: '@'
     , compileDebug: false
     , client: true
     });
-    var str = fn.toString();
-    eval('var preFn = ' + str);
+    str = fn.toString();
+    eval('preFn = ' + str);
     assert.ok(!str.match(/__filename/));
     assert.doesNotThrow(function () {
       preFn({pets: users});
     });
-  })
-})
+  });
+});
 
 suite('comments', function () {
   test('fully render with comments removed', function () {
     assert.equal(ejs.render(fixture('comments.ejs')),
         fixture('comments.html'));
-  })
-})
+  });
+});
 
 suite('require', function () {
   test('allow ejs templates to be required as node modules', function () {
@@ -296,5 +308,6 @@ suite('require', function () {
         , template = require(__dirname + '/fixtures/menu.ejs');
       assert.equal(template({filename: file, pets: users}),
         fixture('menu.html'));
-  })
-})
+  });
+});
+
