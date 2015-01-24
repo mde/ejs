67bba96d2eed1755a924ada900091e6ef1989690
Timothy Gu <timothygu99@gmail.com>
2015-01-23 22:11:58 -0800

    Add test for include() to make sure it is dynamic

--- test/ejs.js
+++ test/ejs.js
@@ -657,6 +657,18 @@ suite('preprocessor include', function () {
     throw new Error('no error reported when there should be');
   });
 
+  test('is dynamic', function () {
+    fs.writeFileSync(__dirname + '/tmp/include.ejs', '<p>Old</p>');
+    var file = 'test/fixtures/include_cache.ejs'
+      , options = {filename: file}
+      , out = ejs.render(fixture('include_cache.ejs'), {}, options);
+    assert.equal(out, '<p>Old</p>');
+
+    fs.writeFileSync(__dirname + '/tmp/include.ejs', '<p>New</p>');
+    out = ejs.render(fixture('include_cache.ejs'), {}, options);
+    assert.equal(out, '<p>New</p>');
+  });
+
   test('support caching (pass 1)', function () {
     fs.writeFileSync(__dirname + '/tmp/include_preprocessor.ejs', '<p>Old</p>');
     var file = 'test/fixtures/include_preprocessor_cache.ejs'
