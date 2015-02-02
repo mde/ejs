384443eeccee44402de62253434c3360d95ac092
mde <mde@fleegix.org>
2015-02-01 20:50:18 -0800

    Begin work on doc site

---
+++ site/assets/js/main.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function($) {
+
+    /* ======= Scrollspy ======= */
+    $('body').scrollspy({ target: '#header', offset: 400});
+    
+    /* ======= Fixed header when scrolled ======= */
+    
+    $(window).bind('scroll', function() {
+         if ($(window).scrollTop() > 50) {
+             $('#header').addClass('navbar-fixed-top');
+         }
+         else {
+             $('#header').removeClass('navbar-fixed-top');
+         }
+    });
+   
+    /* ======= ScrollTo ======= */
+    $('a.scrollto').on('click', function(e){
+        
+        //store hash
+        var target = this.hash;
+                
+        e.preventDefault();
+        
+		$('body').scrollTo(target, 800, {offset: -70, 'axis':'y', easing:'easeOutQuad'});
+        //Collapse mobile menu after clicking
+		if ($('.navbar-collapse').hasClass('in')){
+			$('.navbar-collapse').removeClass('in').addClass('collapse');
+		}
+		
+	});
+
+});
