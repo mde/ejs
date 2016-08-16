54d5ab0b3fe1e00998f6000b8caa7fb633512290
RyanZim <RyanZim@users.noreply.github.com>
2016-08-16 16:02:07 -0400

    Add lint script and fix lint errors in lib/utils.js & examples/
    Add .eslintignore

--- examples/functions.js
+++ examples/functions.js
@@ -2,17 +2,17 @@
  * Believe it or not, you can declare and use functions in EJS templates too.
  */
 
-var ejs = require('../')
-  , read = require('fs').readFileSync
-  , join = require('path').join
-  , path = join(__dirname, '/functions.ejs')
-  , data = {
-      users: [
-        { name: 'Tobi', age: 2, species: 'ferret' }
-      , { name: 'Loki', age: 2, species: 'ferret' }
-      , { name: 'Jane', age: 6, species: 'ferret' }
-      ]
-    };
+var ejs = require('../');
+var read = require('fs').readFileSync;
+var join = require('path').join;
+var path = join(__dirname, '/functions.ejs');
+var data = {
+  users: [
+    { name: 'Tobi', age: 2, species: 'ferret' },
+    { name: 'Loki', age: 2, species: 'ferret' },
+    { name: 'Jane', age: 6, species: 'ferret' }
+  ]
+};
 
 var ret = ejs.compile(read(path, 'utf8'), {filename: path})(data);
 
