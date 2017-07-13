95d88f49197f4d19c4f0a420500545827dfac517
m <m>
2017-07-13 13:52:07 +0100

    sample code for mixin

---
+++ test/ejs-mixin.js
@@ -0,0 +1,64 @@
+/* jshint mocha: true */
+/* eslint-env node, mocha */
+
+/**
+ * Module dependencies.
+ */
+
+var ejs = require('..');
+ejs.enableMixin();
+var fs = require('fs');
+var read = fs.readFileSync;
+var assert = require('assert');
+var path = require('path');
+var LRU = require('lru-cache');
+
+try {
+  fs.mkdirSync(__dirname + '/tmp');
+} catch (ex) {
+  if (ex.code !== 'EEXIST') {
+    throw ex;
+  }
+}
+
+// From https://gist.github.com/pguillory/729616
+function hook_stdio(stream, callback) {
+  var old_write = stream.write;
+
+  stream.write = (function() {
+    return function(string, encoding, fd) {
+      callback(string, encoding, fd);
+    };
+  })(stream.write);
+
+  return function() {
+    stream.write = old_write;
+  };
+}
+
+/**
+ * Load fixture `name`.
+ */
+
+function fixture(name) {
+  return read('test/fixtures/' + name, 'utf8');
+}
+
+/**
+ * User fixtures.
+ */
+
+var users = [];
+users.push({name: 'geddy'});
+users.push({name: 'neil'});
+users.push({name: 'alex'});
+
+suite('ejs.mixin', function () {
+
+  test('work when nested', function () {
+    var file = 'test/fixtures/mixin.ejs';
+    ejs.render(fixture('mixin.ejs'), {pets: users}, {filename: file, cache:true,}),        fixture('mixin.html');
+  });
+  
+});
+
