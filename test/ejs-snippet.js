419a2846b727f39f5bf87a9ed86ff7dfbe2cad4f
m <m>
2017-07-28 23:10:45 +0100

    tests / fix

--- test/ejs-snippet.js
+++ test/ejs-snippet.js
@@ -44,20 +44,71 @@ function fixture(name) {
   return read('test/fixtures/' + name, 'utf8');
 }
 
-/**
- * User fixtures.
- */
-
-var users = [];
-users.push({name: 'geddy'});
-users.push({name: 'neil'});
-users.push({name: 'alex'});
 
 suite('ejs.snippet', function () {
 
-  test('work when nested', function () {
+  test('snippet', function () {
+    var fn = ejs.compile('<%*snippet hello%>world<%*/snippet%>123 <%-snippet("hello")%> 456');
+    assert.equal(fn(), '123 world 456');
+  });
+
+  test('snippet hoisted', function () {
+    var fn = ejs.compile('123 <%-snippet("hello")%> 456<%*snippet hello%>world<%*/snippet%>');
+    assert.equal(fn(), '123 world 456');
+  });
+
+  test('snippet sees locals before modifications', function () {
+    var fn = ejs.compile('<%locals.name="planet"%><%*snippet hello%><%-name%><%*/snippet%>123 <%-snippet("hello")%> 456');
+    assert.equal(fn({name: 'world'}), '123 world 456');
+  });
+
+  test('snippet modifies its own locals only', function () {
+    var fn = ejs.compile('<%*snippet hello%><%locals.name="planet"%><%-name%><%*/snippet%>123 <%-snippet("hello")%> 456 <%-name%>');
+    assert.equal(fn({name: 'world'}), '123 planet 456 world');
+  });
+
+  test('snippet take arguments merged to locals', function () {
+    var fn = ejs.compile('<%*snippet hello%><%-name%> <%locals.name="planet"%><%-name%> <%-foo%><%*/snippet%>123 <%-snippet("hello", {name:"sun"})%> 456 <%-name%>');
+    assert.equal(fn({name: 'world', foo: 'bar'}), '123 sun planet bar 456 world');
+  });
+
+  test('snippet can call itself recursive', function () {
+    var fn = ejs.compile('<%* snippet hello %><%-foo%><%if(foo) {%><%- snippet("hello", {foo: foo-1})%><%}%><%* /snippet %>abc <%-snippet("hello", { foo: 5 })%> def');
+    assert.equal(fn(), 'abc 543210 def');
+  });
+  
+  test('snippet can call each other / recurse', function () {
+    var fn = ejs.compile('<%* snippet hello %><%-foo%><%if(foo) {%><%- snippet("next", {foo: foo})%><%}%><%* /snippet %><%* snippet next %><%-snippet("hello", {foo: foo-1})%><%* /snippet %>abc <%-snippet("hello", { foo: 5 })%> def');
+    assert.equal(fn(), 'abc 543210 def');
+  });
+
+  test('snippet defined in include, used in main', function () {
     var file = 'test/fixtures/snippet.ejs';
-    assert.equal(ejs.render(fixture('snippet.ejs'), {pets: users}, {filename: file, cache:true,}),        fixture('snippet.html'));
+    assert.equal(ejs.render(fixture('snippet.ejs'), {}, {filename: file, cache:true,}),        fixture('snippet.html'));
+  });
+  
+  test('snippet defined in main, used in include', function () {
+    var file = 'test/fixtures/snippet-foo.ejs';
+    assert.equal(ejs.render('<%*snippet hello%>world<%*/snippet%>123 <%-include("includes/snippet-inc2", {snip: "hello"})%> 456',
+      {}, 
+      {filename: file, cache:false,}),
+    '123 world 456');
+  });
+  
+  test('snippet defined in main, changed by include', function () {
+    var file = 'test/fixtures/snippet-foo.ejs';
+    assert.equal(ejs.render('<%*snippet bar%>world<%*/snippet%>123 <%for(var a in [1,2]){%><%-snippet("bar")%> <%-include("includes/snippet-inc")%><%}%>456',
+      {}, 
+      {filename: file, cache:false,}),
+    '123 world \ngot bar \n456');
+  });
+  
+  test('snippet defined in main, changed by include within the snippet (self replace)', function () {
+    var file = 'test/fixtures/snippet-foo.ejs';
+    assert.equal(ejs.render('<%*snippet bar%>world<%-include("includes/snippet-inc")%><%*/snippet%>123 <%for(var a in [1,2]){%><%-snippet("bar")%> <%}%>456',
+      {}, 
+      {filename: file, cache:false,}),
+    '123 world\n got bar 456');
   });
   
 });
