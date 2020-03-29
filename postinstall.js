122035987aad7629a1576e6452646b558b2ed84b
mde <mde@fleegix.org>
2020-03-29 11:27:04 -0700

    Clearer conditional in postinstall

--- postinstall.js
+++ postinstall.js
@@ -10,7 +10,7 @@ let envDisable = isTrue(process.env.DISABLE_OPENCOLLECTIVE) || isTrue(process.en
 let logLevel = process.env.npm_config_loglevel;
 let logLevelDisplay = ['silent', 'error', 'warn'].indexOf(logLevel) > -1;
 
-if (!envDisable && !logLevelDisplay) {
+if (!(envDisable || logLevelDisplay)) {
   console.log('Thank you for installing \u001b[35mEJS\u001b[0m: built with the \u001b[32mJake\u001b[0m JavaScript build tool (\u001b[32mhttps://jakejs.com/\u001b[0m\)\n');
 }
 
