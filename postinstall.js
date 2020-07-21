a807590b87c2d8f760cae88f1bb9b9e0fb1b3136
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2020-07-22 01:50:00 +0200

    fix(postinstall): Use `var` instead of `let` to support ancient node

--- postinstall.js
+++ postinstall.js
@@ -6,12 +6,10 @@ function isTrue(value) {
   return !!value && value !== '0' && value !== 'false';
 }
 
-let envDisable = isTrue(process.env.DISABLE_OPENCOLLECTIVE) || isTrue(process.env.CI);
-let logLevel = process.env.npm_config_loglevel;
-let logLevelDisplay = ['silent', 'error', 'warn'].indexOf(logLevel) > -1;
+var envDisable = isTrue(process.env.DISABLE_OPENCOLLECTIVE) || isTrue(process.env.CI);
+var logLevel = process.env.npm_config_loglevel;
+var logLevelDisplay = ['silent', 'error', 'warn'].indexOf(logLevel) > -1;
 
-if (!envDisable && !logLevelDisplay) {
-  console.log('Thank you for installing \u001b[35mEJS\u001b[0m: built with the \u001b[32mJake\u001b[0m JavaScript build tool (\u001b[32mhttps://jakejs.com/\u001b[0m)\n');
+if (!(envDisable || logLevelDisplay)) {
+  console.log('Thank you for installing \u001b[35mEJS\u001b[0m: built with the \u001b[32mJake\u001b[0m JavaScript build tool (\u001b[32mhttps://jakejs.com/\u001b[0m\)\n');
 }
-
-
