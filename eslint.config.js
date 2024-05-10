48eab2207c98f75da248cf23c7069e2e862fe343
Jumpei Ogawa <git@phanective.org>
2024-05-10 12:38:09 +0900

    Lint

--- eslint.config.js
+++ eslint.config.js
@@ -5,45 +5,45 @@ export default [
     files: [ '*.js', '*.mjs', '*.cjs' ],
     languageOptions: {
       globals: {
-        "suite": "readonly",
-        "test": "readonly"
+        suite: 'readonly',
+        test: 'readonly'
       },
-      "parserOptions": {
-        "ecmaVersion": 6,
-        "requireConfigFile": false,
+      parserOptions: {
+        ecmaVersion: 6,
+        requireConfigFile: false,
       },
     },
     plugins: {
       '@stylistic/js': stylisticJs
     },
     rules: {
-        "@stylistic/js/linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "@stylistic/js/no-trailing-spaces": 2,
-        "@stylistic/js/indent": [
-          "error",
-          2
-        ],
-        "@stylistic/js/quotes": [
-            "error",
-            "single",
-            {
-              "avoidEscape": true,
-              "allowTemplateLiterals": true
-            }
-        ],
-        "@stylistic/js/semi": [
-            "error",
-            "always"
-        ],
-        "@stylistic/js/comma-style": [
-            "error",
-            "last"
-        ],
-        "no-console": 0,
-        "no-useless-escape": 0
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'unix'
+      ],
+      '@stylistic/js/no-trailing-spaces': 2,
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single',
+        {
+          'avoidEscape': true,
+          'allowTemplateLiterals': true
+        }
+      ],
+      '@stylistic/js/semi': [
+        'error',
+        'always'
+      ],
+      '@stylistic/js/comma-style': [
+        'error',
+        'last'
+      ],
+      'no-console': 0,
+      'no-useless-escape': 0
     }
   },
   {
@@ -58,6 +58,4 @@ export default [
       sourceType: 'commonjs',
     },
   },
-]
-
-
+];
